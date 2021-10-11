7f98b091b31ee7d40fed8954a447551cbf948820
Waleed Iqbal <waleedshykh@hotmail.com>
2021-10-11 11:51:16 +0500

    u

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module SlackVoice
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.web_console.permissions = '119.155.21.102'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.web_console.permissions = '119.155.21.102'
+  #config.web_console.permissions = '119.155.21.102'
     
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
