17a2030d9349c9c74edcfc67301e0c42fba8f068
Waleed Iqbal <waleedshykh@hotmail.com>
2021-10-21 16:10:13 +0500

    Update slack_auth_controller.rb

--- app/controllers/slack_auth_controller.rb
+++ app/controllers/slack_auth_controller.rb
@@ -10,7 +10,7 @@ class SlackAuthController < ApplicationController
 
     pp response.body
     create_user  JSON.parse(response.body)
-    redirect_to "app.slack.com"
+    redirect_to "https://app.slack.com"
   end
 
   def create
